898a5d0227f3d644fc3a8f388da6e0b5454b798b
Prashanthi-Kasula <prashanthi.kasula99@gmail.com>
2023-04-15 14:19:20 +0000

    simple-todos

---
+++ src/components/SimpleTodos/index.js
@@ -0,0 +1,87 @@
+import {Component} from 'react'
+
+import TodoItem from '../TodoItem'
+
+import './index.css'
+
+const initialTodosList = [
+  {
+    id: 1,
+    title: 'Book the ticket for today evening',
+  },
+  {
+    id: 2,
+    title: 'Rent the movie for tomorrow movie night',
+  },
+  {
+    id: 3,
+    title: 'Confirm the slot for the yoga session tomorrow morning',
+  },
+  {
+    id: 4,
+    title: 'Drop the parcel at Bloomingdale',
+  },
+  {
+    id: 5,
+    title: 'Order fruits on Big Basket',
+  },
+  {
+    id: 6,
+    title: 'Fix the production issue',
+  },
+  {
+    id: 7,
+    title: 'Confirm my slot for Saturday Night',
+  },
+  {
+    id: 8,
+    title: 'Get essentials for Sunday car wash',
+  },
+]
+
+class SimpleTodos extends Component {
+  state = {
+    todosList: initialTodosList,
+  }
+
+  deleteTodo = id => {
+    const {todosList} = this.state
+    const updatedTodosList = todosList.filter(eachTodo => eachTodo.id !== id)
+
+    this.setState({
+      todosList: updatedTodosList,
+    })
+  }
+
+  render() {
+    const {todosList} = this.state
+
+    return (
+      <div className="app-container">
+        {/* <div className="count-container">
+          <h1 className="count">
+            No. of Todos: <span className="number">{todosList.length}</span>
+          </h1>
+        </div> */}
+
+        <div className="todos-container">
+          <h1 className="heading">
+            Simple Todos <span className="count">{todosList.length}</span>
+          </h1>
+
+          <ul className="list">
+            {todosList.map(eachTodo => (
+              <TodoItem
+                key={eachTodo.id}
+                todoDetails={eachTodo}
+                deleteTodo={this.deleteTodo}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SimpleTodos
